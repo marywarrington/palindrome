2537f393c1671749d98c04355f0e7edd06437f85
Blair Peterson and Mary Warrington <blairchalmers@gmail.com>
2016-01-20 11:55:11 -0800

    Add beginning loop functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,41 @@
-var isVowel = function(word) {
-  var vowels = ["a", "e", "i", "o", "u"];
-  if (vowels.includes(word[0])) {
-    return true;
-  } else {
-    return false;
-  }
+var makeArray = function(word) {
+  var letters = word.split("");
+  return letters;
 };
 
 var pigLatin = function(word) {
-    if (isVowel(word)) {
-      return (word + "ay");
-    } else if ((!isVowel(word[0])) && (isVowel(word[1]))) {
-      return ((word.substring(1,word.length)) + (word.substring(0,1)) + ("ay"));
-    } else if ((!isVowel(word[0,1])) && (isVowel(word[2]))) {
-      return ((word.substring(2,word.length)) + (word.substring(0,2)) + ("ay"));
-    } else if ((!isVowel(word[0,1,2])) && (isVowel(word[3]))) {
-      return ((word.substring(3,word.length)) + (word.substring(0,3)) + ("ay"));
-    } else if ((!isVowel(word[0,1,2,3])) && (isVowel(word[4]))) {
-      return ((word.substring(4,word.length)) + (word.substring(0,4)) + ("ay"));
-    } else if ((!isVowel(word[0,1,2,3,4])) && (isVowel(word[5]))) {
-      return ((word.substring(5,word.length)) + (word.substring(0,5)) + ("ay"));
-    } else if ((!isVowel(word[0,1,2,3,4,5])) && (isVowel(word[6]))) {
-      return ((word.substring(6,word.length)) + (word.substring(0,6)) + ("ay"));
-    }
-};
+  var array = makeArray(word)
+}
+
+
+
+
+// var isVowel = function(word) {
+//   var vowels = ["a", "e", "i", "o", "u"];
+//   if (vowels.includes(word[0])) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// var pigLatin = function(word) {
+//     if (isVowel(word)) {
+//       return (word + "ay");
+//     } else if ((!isVowel(word[0])) && (isVowel(word[1]))) {
+//       return ((word.substring(1,word.length)) + (word.substring(0,1)) + ("ay"));
+//     } else if ((!isVowel(word[0,1])) && (isVowel(word[2]))) {
+//       return ((word.substring(2,word.length)) + (word.substring(0,2)) + ("ay"));
+//     } else if ((!isVowel(word[0,1,2])) && (isVowel(word[3]))) {
+//       return ((word.substring(3,word.length)) + (word.substring(0,3)) + ("ay"));
+//     } else if ((!isVowel(word[0,1,2,3])) && (isVowel(word[4]))) {
+//       return ((word.substring(4,word.length)) + (word.substring(0,4)) + ("ay"));
+//     } else if ((!isVowel(word[0,1,2,3,4])) && (isVowel(word[5]))) {
+//       return ((word.substring(5,word.length)) + (word.substring(0,5)) + ("ay"));
+//     } else if ((!isVowel(word[0,1,2,3,4,5])) && (isVowel(word[6]))) {
+//       return ((word.substring(6,word.length)) + (word.substring(0,6)) + ("ay"));
+//     }
+// };
 
 //
 // for (var i = 0;i < word.length; i++) {
--- specs/specs.js
+++ specs/specs.js
@@ -1,29 +1,36 @@
-describe("isVowel",function() {
-  it("returns true for a word beginning with a vowel", function() {
-    expect(isVowel("unusual")).to.equal(true);
-  });
-  it("returns false for a word not beginning with a vowel", function() {
-  expect(isVowel("carrot")).to.equal(false);
+
+describe("makeArray", function() {
+  it("returns an array of the letters of a given word", function() {
+    expect(makeArray("happy")).to.eql([ "h", "a", "p", "p", "y" ]);
   });
 });
 
-describe("pigLatin", function() {
-  it("adds 'ay' to the end of a word beginning with a vowel", function(){
-    expect(pigLatin("and")).to.equal("anday");
-  });
-  it("removes first letter and appends it to the end of the word, along with 'ay'", function(){
-    expect(pigLatin("bus")).to.equal("usbay");
-  });
-  it("removes first two letters and appends them to the end of the word, along with 'ay'", function(){
-    expect(pigLatin("train")).to.equal("aintray");
-  });
-  it("removes first three letters and appends them to the end of the word, along with 'ay'", function(){
-    expect(pigLatin("three")).to.equal("eethray");
-  });
-  it("removes first four letters and appends them to the end of the word, along with 'ay'", function(){
-    expect(pigLatin("mystery")).to.equal("erymystay");
-  });
-  it("removes first five letters and appends them to the end of the word, along with 'ay'", function(){
-    expect(pigLatin("bcdfgein")).to.equal("einbcdfgay");
-  });
-});
+// describe("isVowel",function() {
+//   it("returns true for a word beginning with a vowel", function() {
+//     expect(isVowel("unusual")).to.equal(true);
+//   });
+//   it("returns false for a word not beginning with a vowel", function() {
+//   expect(isVowel("carrot")).to.equal(false);
+//   });
+// });
+//
+// describe("pigLatin", function() {
+//   it("adds 'ay' to the end of a word beginning with a vowel", function(){
+//     expect(pigLatin("and")).to.equal("anday");
+//   });
+//   it("removes first letter and appends it to the end of the word, along with 'ay'", function(){
+//     expect(pigLatin("bus")).to.equal("usbay");
+//   });
+//   it("removes first two letters and appends them to the end of the word, along with 'ay'", function(){
+//     expect(pigLatin("train")).to.equal("aintray");
+//   });
+//   it("removes first three letters and appends them to the end of the word, along with 'ay'", function(){
+//     expect(pigLatin("three")).to.equal("eethray");
+//   });
+//   it("removes first four letters and appends them to the end of the word, along with 'ay'", function(){
+//     expect(pigLatin("mystery")).to.equal("erymystay");
+//   });
+//   it("removes first five letters and appends them to the end of the word, along with 'ay'", function(){
+//     expect(pigLatin("bcdfgein")).to.equal("einbcdfgay");
+//   });
+// });
